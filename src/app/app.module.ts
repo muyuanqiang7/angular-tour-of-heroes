bfcbfce483437f56b43bd5cb6749775e983e139f
yuanqiang.mou <yuanqiang.mou@funi365.com>
2017-06-26 17:27:25 +0800

    angular2 http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import {AppComponent} from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
 import {HeroService} from './hero.service';
-import {RouterModule} from '@angular/router';
 import {DashboardComponent} from './dashboard.component'
-import {HttpModule} from '@angular/http'
+import {HttpModule} from '@angular/http';
+import {AppRoutingModule} from './app-routing.moudle'
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -20,22 +22,8 @@ import {HttpModule} from '@angular/http'
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      }, {
-        path: 'dashboard',
-        component: DashboardComponent
-      }, {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      }, {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
