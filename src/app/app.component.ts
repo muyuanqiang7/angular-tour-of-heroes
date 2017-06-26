370e3a136cf64dbaffad96bd1048c0ccbbc6a4c1
yuanqiang.mou <yuanqiang.mou@funi365.com>
2017-06-26 15:39:04 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['./app.component.css'],
+  template: `<h1>{{title}}</h1>
+  <nav>
+    <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+  </nav>
+  <router-outlet></router-outlet>`,
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent} from './heroes.component';
+import {HeroService} from './hero.service';
+import {RouterModule} from '@angular/router';
+import {DashboardComponent} from './dashboard.component'
+import {HttpModule} from '@angular/http'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }, {
+        path: 'dashboard',
+        component: DashboardComponent
+      }, {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }, {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }
+    ])
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
